21aa4c1b9431438e8e0258c7c6a12fe04eb84640
Webie <90978643+vimal-oneway@users.noreply.github.com>
2023-04-19 14:01:54 +0530

    home page done

--- server/build/middleware/catchAsyncErrors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.catchAsyncErrors = void 0;
-var catchAsyncErrors = function (func) { return function (req, res, next) {
-    return Promise
-        .resolve(func(req, res, next))
-        .catch(next);
-}; };
-exports.catchAsyncErrors = catchAsyncErrors;
--- server/build/routes/loginRoutes.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-var express_1 = require("express");
-var router = (0, express_1.Router)();
-exports.router = router;
